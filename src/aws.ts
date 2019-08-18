8d1620fb9b43812339b94e9cdd656ee2ff9aa9de
mbromberek <mikebromberek@gmail.com>
2019-08-18 14:05:43 -0500

    updated authentication configurations

--- src/aws.ts
+++ src/aws.ts
@@ -4,7 +4,7 @@ import { config } from './config/config';
 const c = config.dev;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
@@ -23,12 +23,13 @@ export const s3 = new AWS.S3({
 export function getGetSignedUrl( key: string ): string{
 
   const signedUrlExpireSeconds = 60 * 5
+  const param = {
+  	Bucket: c.aws_media_bucket, 
+  	Key: key, 
+  	Expires: signedUrlExpireSeconds
+  };
 
-    const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
-        Key: key,
-        Expires: signedUrlExpireSeconds
-      });
+  const url = s3.getSignedUrl('getObject', param);
 
     return url;
 }
